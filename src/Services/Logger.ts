Justin-Credible/Ionic-TypeScript-Starter

commit c7ac45311a7cd503c11bcd04b8913a4963b6082f

Improve logging service #28

src/Services/Logger.ts
@@ -26,44 +26,240 @@
         private _logs: Models.LogEntry[];
         private _maxLogEntries = 20;
 
+        //#region Logging Convenience Methods
+
+        /**
+         * Used to log debbuging information (like method timings etc).
+         * 
+         * @param tagPrefix The prefix for the log entries tag (normally the ID of the service or controller).
+         * @param tag The tag for the log (normally the name of the method).
+         * @param message The descriptive text for the log entry.
+         * @param metadata An optional object to be logged with the log entry.
+         */
         public trace(tagPrefix: string, tag: string, message: string, metadata?: any): void {
             this.log(Models.LogLevel.TRACE, tagPrefix, tag, message, metadata);
         }
 
+        /**
+         * Used to log debugging information.
+         * 
+         * @param tagPrefix The prefix for the log entries tag (normally the ID of the service or controller).
+         * @param tag The tag for the log (normally the name of the method).
+         * @param message The descriptive text for the log entry.
+         * @param metadata An optional object to be logged with the log entry.
+         */
         public debug(tagPrefix: string, tag: string, message: string, metadata?: any): void {
             this.log(Models.LogLevel.DEBUG, tagPrefix, tag, message, metadata);
         }
 
+        /**
+         * Used to log an informational message.
+         * 
+         * @param tagPrefix The prefix for the log entries tag (normally the ID of the service or controller).
+         * @param tag The tag for the log (normally the name of the method).
+         * @param message The descriptive text for the log entry.
+         * @param metadata An optional object to be logged with the log entry.
+         */
         public info(tagPrefix: string, tag: string, message: string, metadata?: any): void {
             this.log(Models.LogLevel.INFO, tagPrefix, tag, message, metadata);
         }
 
+        /**
+         * Used to log a warning.
+         * 
+         * @param tagPrefix The prefix for the log entries tag (normally the ID of the service or controller).
+         * @param tag The tag for the log (normally the name of the method).
+         * @param message The descriptive text for the log entry.
+         * @param metadata An optional object to be logged with the log entry.
+         */
         public warn(tagPrefix: string, tag: string, message: string, metadata?: any): void {
             this.log(Models.LogLevel.WARN, tagPrefix, tag, message, metadata);
         }
 
+        /**
+         * Used to log an error.
+         * 
+         * @param tagPrefix The prefix for the log entries tag (normally the ID of the service or controller).
+         * @param tag The tag for the log (normally the name of the method).
+         * @param message The descriptive text for the log entry.
+         * @param metadata An optional object to be logged with the log entry.
+         */
         public error(tagPrefix: string, tag: string, message: string, metadata?: any): void {
             this.log(Models.LogLevel.ERROR, tagPrefix, tag, message, metadata);
         }
 
+        /**
+         * Used to log a fatal error.
+         * 
+         * @param tagPrefix The prefix for the log entries tag (normally the ID of the service or controller).
+         * @param tag The tag for the log (normally the name of the method).
+         * @param message The descriptive text for the log entry.
+         * @param metadata An optional object to be logged with the log entry.
+         */
         public fatal(tagPrefix: string, tag: string, message: string, metadata?: any): void {
             this.log(Models.LogLevel.FATAL, tagPrefix, tag, message, metadata);
         }
 
+        //#endregion
+
+        //#region Public Methods
+
+        /**
+         * Used to clear the logs that are current in memory.
+         */
         public clear(): void {
             this._logs = [];
         }
 
+        /**
+         * Used to return all of the logs that are currently in memory.
+         * 
+         * @returns An array of log entries.
+         */
         public get logs(): Models.LogEntry[] {
             return this._logs;
         }
 
+        /**
+         * Used to get a single log entry by log ID.
+         * 
+         * @param id The log ID of the log to retrieve.
+         * @returns A single log entry with the given ID.
+         */
         public getLog(id: string): Models.LogEntry {
             return _.find(this._logs, (logEntry: Models.LogEntry) => {
                 return logEntry.id === id;
             });
         }
 
+        /**
+         * A helper used to get an icon name for the given log level.
+         * 
+         * @param level The level of the log to get an icon name for.
+         * @returns A string of an icon name for the given log level.
+         */
+        public getIconForLevel(level: number): string {
+
+            if (level == null) {
+                return "";
+            }
+
+            switch (level) {
+                case Models.LogLevel.TRACE:
+                    return "ion-code-working";
+                    break;
+                case Models.LogLevel.DEBUG:
+                    return "ion-bug";
+                    break;
+                case Models.LogLevel.INFO:
+                    return "ion-information-circled";
+                    break;
+                case Models.LogLevel.WARN:
+                    return "ion-alert-circled";
+                    break;
+                case Models.LogLevel.ERROR:
+                    return "ion-alert";
+                    break;
+                case Models.LogLevel.FATAL:
+                    return "ion-nuclear";
+                    break;
+                default:
+                    return "ion-alert";
+                    break;
+            }
+        }
+
+        /**
+         * A helper used to get a color in hex value for the given log level.
+         * 
+         * @param level The level of the log to get a color for.
+         * @returns A hex value of a color for the given log level.
+         */
+        public getColorForLevel(level: number): string {
+
+            if (level == null) {
+                return "";
+            }
+
+            switch (level) {
+                case Models.LogLevel.TRACE:
+                    return "#551A8B"; // Purple
+                    break;
+                case Models.LogLevel.DEBUG:
+                    return "#000080"; // Navy
+                    break;
+                case Models.LogLevel.INFO:
+                    return "#000000"; // Black
+                    break;
+                case Models.LogLevel.WARN:
+                    return "#ff8000"; // Orange
+                    break;
+                case Models.LogLevel.ERROR:
+                    return "#ff0000"; // Red
+                    break;
+                case Models.LogLevel.FATAL:
+                    return "#ff0000"; // Red
+                    break;
+                default:
+                    return "#000000"; // Black
+                    break;
+            }
+        }
+
+        /**
+         * A helper used to get friendly name for display for the given log level.
+         * 
+         * @param level The level of the log to get a display name for.
+         * @returns A display name of for the given log level.
+         */
+        public getDisplayLevelForLevel(level: number): string {
+
+            if (level == null) {
+                return "";
+            }
+
+            switch (level) {
+                case Models.LogLevel.TRACE:
+                    return "Trace";
+                    break;
+                case Models.LogLevel.DEBUG:
+                    return "Debug";
+                    break;
+                case Models.LogLevel.INFO:
+                    return "Info";
+                    break;
+                case Models.LogLevel.WARN:
+                    return "Warning";
+                    break;
+                case Models.LogLevel.ERROR:
+                    return "Error";
+                    break;
+                case Models.LogLevel.FATAL:
+                    return "Fatal";
+                    break;
+                default:
+                    return "Unknown";
+                    break;
+            }
+        }
+
+        //#endregion
+
+        //#region Base logging method
+
+        /**
+         * Logs a log entry.
+         * 
+         * Currently logs to an in-memory array whose max size is _maxLogEntries.
+         * 
+         * This can easily be modified to use a third party logging service (eg Ouralabs).
+         * 
+         * @param logLevel The severity of the log (see Models.LogLevel for possible values).
+         * @param tagPrefix The prefix for the log entries tag (normally the ID of the service or controller).
+         * @param tag The tag for the log (normally the name of the method).
+         * @param message The descriptive text for the log entry.
+         * @param metadata An optional object to be logged with the log entry.
+         */
         private log(logLevel: Models.LogLevel, tagPrefix: string, tag: string, message: string, metadata?: any): void {
 
             if (logLevel == null) {
@@ -85,7 +281,7 @@
             var logEntry = new Models.LogEntry();
 
             logEntry.id = this.Utilities.generateGuid();
-            logEntry.level = Models.LogLevel.TRACE;
+            logEntry.level = logLevel;
             logEntry.tag = tagPrefix ? tagPrefix + "." + tag : tag;
             logEntry.message = message;
             logEntry.metadata = metadata;
@@ -122,5 +318,7 @@
                     break;
             }
         }
+
+        //#endregion
     }
 }

src/ViewModels/Settings/LogsListViewModel.ts
@@ -1,7 +1,15 @@
 module JustinCredible.SampleApp.ViewModels {
 
     export class LogsListViewModel {
+
         public logs: { [day: string]: ViewModels.LogEntryViewModel[] };
+
+        public showTrace: boolean;
+        public showDebug: boolean;
+        public showInfo: boolean;
+        public showWarn: boolean;
+        public showError: boolean;
+        public showFatal: boolean;
 
         constructor() {
             this.logs = {};

src/Controllers/Settings/LogEntryViewModel.ts
@@ -6,5 +6,7 @@ module JustinCredible.SampleApp.ViewModels {
         public date: string;
         public time: string;
         public levelDisplay: string;
+        public color: string;
+        public formattedMetadata: string;
     }
 }

src/Application.ts
@@ -530,7 +530,7 @@ module JustinCredible.SampleApp.Application {
         // Save off the current route so we can use it for logging.
         currentRoute = newRoute;
 
-        services.Logger.debug("Application", "angular_locationChangeStart", "Angular location changed.", { oldRoute: oldRoute, newRoute: newRoute });
+        services.Logger.trace("Application", "angular_locationChangeStart", "Angular location changed.", { oldRoute: oldRoute, newRoute: newRoute });
     };
 
     /**
@@ -554,7 +554,7 @@ module JustinCredible.SampleApp.Application {
             services.Plugins.progressIndicator.hide();
         }
         catch (ex) {
-            console.warn("There was a problem alerting the user to an Angular error; falling back to a standard alert().", ex);
+            services.Logger.warn("Application", "window_onerror", "There was a problem alerting the user to an Angular error; falling back to a standard alert().", ex);
             alert("An error has occurred; please try again.");
         }
     }
@@ -587,7 +587,7 @@ module JustinCredible.SampleApp.Application {
             services.Plugins.progressIndicator.hide();
         }
         catch (ex) {
-            console.warn("There was a problem alerting the user to an Angular error; falling back to a standard alert().", ex);
+            services.Logger.warn("Application", "angular_exceptionHandler", "There was a problem alerting the user to an Angular error; falling back to a standard alert().", ex);
             alert("An error has occurred; please try again.");
         }
     }

src/Controllers/Settings/DeveloperController.ts
@@ -9,7 +9,6 @@
         public static get $inject(): string[] {
             return [
                 "$scope",
-                "$http",
                 Services.Plugins.ID,
                 Services.Utilities.ID,
                 Services.UiHelper.ID,
@@ -23,7 +22,6 @@
 
         constructor(
             $scope: ng.IScope,
-            private $http: ng.IHttpService,
             private Plugins: Services.Plugins,
             private Utilities: Services.Utilities,
             private UiHelper: Services.UiHelper,

src/Controllers/Settings/LogEntryController.ts
@@ -38,33 +38,20 @@
             super.view_beforeEnter(event, eventArgs);
 
             this.viewModel.logEntry = this.Logger.getLog(this.$stateParams.id);
-            //TODO populate the rest of the properties here. date, time, leveldisplay via logger.getLevelDisplayText()
-            //TODO Format the metadata (pretty json?)
 
-            //TODO: MOve this to logger.getIcon
-            switch (this.viewModel.logEntry.logLevel) {
-                case Models.LogLevel.TRACE:
-                    this.viewModel.icon = "ion-code-working";
-                    break;
-                case Models.LogLevel.DEBUG:
-                    this.viewModel.icon = "ion-bug";
-                    break;
-                case Models.LogLevel.INFO:
-                    this.viewModel.icon = "ion-information-circled";
-                    break;
-                case Models.LogLevel.WARN:
-                    this.viewModel.icon = "ion-alert-circled";
-                    break;
-                case Models.LogLevel.ERROR:
-                    this.viewModel.icon = "ion-alert";
-                    break;
-                case Models.LogLevel.FATAL:
-                    this.viewModel.icon = "ion-nuclear";
-                    break;
-                default:
-                    this.viewModel.icon = "ion-alert";
-                    break;
+            this.viewModel.date = moment(this.viewModel.logEntry.timestamp).format("MMMM Do YYYY");
+            this.viewModel.time = moment(this.viewModel.logEntry.timestamp).format("h:mm:ss a");
+
+            try {
+                this.viewModel.formattedMetadata = JSON.stringify(this.viewModel.logEntry.metadata, null, 4);
             }
+            catch (exception) {
+                this.viewModel.formattedMetadata = "Unable to stringify metadata: " + exception;
+            }
+
+            this.viewModel.icon = this.Logger.getIconForLevel(this.viewModel.logEntry.level);
+            this.viewModel.color = this.Logger.getColorForLevel(this.viewModel.logEntry.level);
+            this.viewModel.levelDisplay = this.Logger.getDisplayLevelForLevel(this.viewModel.logEntry.level);
         }
 
         //#endregion

src/Controllers/Settings/LogsListController.ts
@@ -9,6 +9,7 @@
         public static get $inject(): string[] {
             return [
                 "$scope",
+                "$ionicPopover",
                 Services.Logger.ID,
                 Services.UiHelper.ID
             ];
@@ -16,6 +17,7 @@
 
         constructor(
             $scope: ng.IScope,
+            private $ionicPopover: any,
             private Logger: Services.Logger,
             private UiHelper: Services.UiHelper) {
             super($scope, ViewModels.LogsListViewModel);
@@ -23,19 +25,31 @@
 
         //#endregion
 
+        private _popover: any;
+
         //#region BaseController Overrides
 
         protected view_beforeEnter(event?: ng.IAngularEvent, eventArgs?: Ionic.IViewEventArguments): void {
             super.view_beforeEnter(event, eventArgs);
 
-            this.setupViewModel(this.Logger.logs);
+            this.$ionicPopover.fromTemplateUrl("templates/Settings/Log-Filter-Menu.html", {
+                scope: this.scope
+            }).then((popover: any) => {
+                this._popover = popover;
+            });
+
+            this.viewModel.showError = true;
+            this.viewModel.showWarn = true;
+            this.viewModel.showFatal = true;
+
+            this.populateViewModel(this.Logger.logs);
         }
 
         //#endregion
 
         //#region Private Helper Methods
 
-        private setupViewModel(logEntries: Models.LogEntry[]): void {
+        private populateViewModel(logEntries: Models.LogEntry[]): void {
 
             if (logEntries == null) {
                 logEntries = [];
@@ -52,33 +66,19 @@
                 var formattedDate: string,
                     viewModel: ViewModels.LogEntryViewModel;
 
+                if (!this.isApplicableForCurrentFilter(logEntry)) {
+                    return;
+                }
+
+                viewModel = new ViewModels.LogEntryViewModel();
+
                 // Put the actual log entry into the view model.
                 viewModel.logEntry = logEntry;
 
-                // Determine the icon based on the log level.
-                switch (logEntry.level) {
-                    case Models.LogLevel.TRACE:
-                        viewModel.icon = "ion-code-working";
-                        break;
-                    case Models.LogLevel.DEBUG:
-                        viewModel.icon = "ion-bug";
-                        break;
-                    case Models.LogLevel.INFO:
-                        viewModel.icon = "ion-information-circled";
-                        break;
-                    case Models.LogLevel.WARN:
-                        viewModel.icon = "ion-alert-circled";
-                        break;
-                    case Models.LogLevel.ERROR:
-                        viewModel.icon = "ion-alert";
-                        break;
-                    case Models.LogLevel.FATAL:
-                        viewModel.icon = "ion-nuclear";
-                        break;
-                    default:
-                        viewModel.icon = "ion-alert";
-                        break;
-                }
+                viewModel.time = moment(logEntry.timestamp).format("h:mm:ss a");
+                viewModel.icon = this.Logger.getIconForLevel(logEntry.level);
+                viewModel.color = this.Logger.getColorForLevel(logEntry.level);
+                viewModel.levelDisplay = this.Logger.getDisplayLevelForLevel(logEntry.level);
 
                 // Format the date and time for display.
                 formattedDate = moment(logEntry.timestamp).format("l");
@@ -95,9 +95,67 @@
             });
         }
 
+        private isApplicableForCurrentFilter(logEntry: Models.LogEntry): boolean {
+
+            if (!logEntry || logEntry.level == null) {
+                return true;
+            }
+
+            switch (logEntry.level) {
+                case Models.LogLevel.TRACE:
+                    return this.viewModel.showTrace;
+                case Models.LogLevel.DEBUG:
+                    return this.viewModel.showDebug;
+                case Models.LogLevel.WARN:
+                    return this.viewModel.showWarn;
+                case Models.LogLevel.INFO:
+                    return this.viewModel.showInfo;
+                case Models.LogLevel.ERROR:
+                    return this.viewModel.showError;
+                case Models.LogLevel.FATAL:
+                    return this.viewModel.showFatal;
+                default:
+                    return true;
+            }
+        }
+
         //#endregion
 
         //#region Controller Methods
+
+        protected filter_click(event: ng.IAngularEvent) {
+            this._popover.show(event);
+        }
+
+        protected trace_click(): void {
+            this.viewModel.showTrace = !this.viewModel.showTrace;
+            this.populateViewModel(this.Logger.logs);
+        }
+
+        protected debug_click(): void {
+            this.viewModel.showDebug = !this.viewModel.showDebug;
+            this.populateViewModel(this.Logger.logs);
+        }
+
+        protected info_click(): void {
+            this.viewModel.showInfo = !this.viewModel.showInfo;
+            this.populateViewModel(this.Logger.logs);
+        }
+
+        protected warn_click(): void {
+            this.viewModel.showWarn = !this.viewModel.showWarn;
+            this.populateViewModel(this.Logger.logs);
+        }
+
+        protected error_click(): void {
+            this.viewModel.showError = !this.viewModel.showError;
+            this.populateViewModel(this.Logger.logs);
+        }
+
+        protected fatal_click(): void {
+            this.viewModel.showFatal = !this.viewModel.showFatal;
+            this.populateViewModel(this.Logger.logs);
+        }
 
         protected clear_click() {
             this.UiHelper.confirm("Are you sure you want to clear the logs?", "Clear Logs").then((result: string) => {

src/RouteConfig.ts
@@ -126,6 +126,12 @@ module JustinCredible.SampleApp {
 
             $stateProvider.state("app.log-entry", {
                 url: "/settings/log-entry/:id",
+                params: {
+                    id: {
+                        value: "",
+                        squash: false
+                    }
+                },
                 views: {
                     "root-view": {
                         templateUrl: "templates/Settings/Log-Entry.html",

commit 4e91046c9005e7aa463214d3e1af36a7c1b7d210

fixed lint errors caused by c7ac45311a7cd503c11bcd04b8913a4963b6082f

src/Services/Logger.ts
@@ -147,25 +147,18 @@
             switch (level) {
                 case Models.LogLevel.TRACE:
                     return "ion-code-working";
-                    break;
                 case Models.LogLevel.DEBUG:
                     return "ion-bug";
-                    break;
                 case Models.LogLevel.INFO:
                     return "ion-information-circled";
-                    break;
                 case Models.LogLevel.WARN:
                     return "ion-alert-circled";
-                    break;
                 case Models.LogLevel.ERROR:
                     return "ion-alert";
-                    break;
                 case Models.LogLevel.FATAL:
                     return "ion-nuclear";
-                    break;
                 default:
                     return "ion-alert";
-                    break;
             }
         }
 
@@ -184,25 +177,18 @@
             switch (level) {
                 case Models.LogLevel.TRACE:
                     return "#551A8B"; // Purple
-                    break;
                 case Models.LogLevel.DEBUG:
                     return "#000080"; // Navy
-                    break;
                 case Models.LogLevel.INFO:
                     return "#000000"; // Black
-                    break;
                 case Models.LogLevel.WARN:
                     return "#ff8000"; // Orange
-                    break;
                 case Models.LogLevel.ERROR:
                     return "#ff0000"; // Red
-                    break;
                 case Models.LogLevel.FATAL:
                     return "#ff0000"; // Red
-                    break;
                 default:
                     return "#000000"; // Black
-                    break;
             }
         }
 
@@ -221,25 +207,18 @@
             switch (level) {
                 case Models.LogLevel.TRACE:
                     return "Trace";
-                    break;
                 case Models.LogLevel.DEBUG:
                     return "Debug";
-                    break;
                 case Models.LogLevel.INFO:
                     return "Info";
-                    break;
                 case Models.LogLevel.WARN:
                     return "Warning";
-                    break;
                 case Models.LogLevel.ERROR:
                     return "Error";
-                    break;
                 case Models.LogLevel.FATAL:
                     return "Fatal";
-                    break;
                 default:
                     return "Unknown";
-                    break;
             }
         }
 